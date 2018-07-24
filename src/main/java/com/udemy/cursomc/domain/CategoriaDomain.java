2b9c365dac6006914aa1511894a11fe60f0bd4d8
Munhoz <munhozfontana@gmail.com>
2018-07-24 18:23:26 -0300

    Banco de dados e a criação altomática de dados

--- src/main/java/com/udemy/cursomc/domain/CategoriaDomain.java
+++ src/main/java/com/udemy/cursomc/domain/CategoriaDomain.java
@@ -2,9 +2,19 @@ package com.udemy.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class CategoriaDomain implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -22,6 +32,7 @@ public class CategoriaDomain implements Serializable{
 	public Integer getId() {
 		return id;
 	}
+	
 
 	public void setId(Integer id) {
 		this.id = id;
