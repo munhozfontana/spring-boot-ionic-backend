c4c35de26307dcea1aaaf7307012f5b661e3877a
Munhoz <munhozfontana@gmail.com>
2018-07-27 08:13:53 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/udemy/cursomc/domain/Cliente.java
+++ src/main/java/com/udemy/cursomc/domain/Cliente.java
@@ -16,6 +16,7 @@ import javax.persistence.OneToMany;
 
 import org.apache.logging.log4j.util.Strings;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.udemy.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -30,6 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
