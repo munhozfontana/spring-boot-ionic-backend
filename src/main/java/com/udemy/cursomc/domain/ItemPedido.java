9001741b96bc619c3551715bc27f9425d8c1cdce
Munhoz <munhozfontana@gmail.com>
2018-07-30 01:22:10 -0300

    endPoit pedidos/{id}

--- src/main/java/com/udemy/cursomc/domain/ItemPedido.java
+++ src/main/java/com/udemy/cursomc/domain/ItemPedido.java
@@ -2,23 +2,25 @@ package com.udemy.cursomc.domain;
 
 import java.io.Serializable;
 
-
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-public class ItemPedido  implements Serializable {
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
+
 	public ItemPedido() {
-		
+
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -29,11 +31,12 @@ public class ItemPedido  implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
@@ -94,7 +97,5 @@ public class ItemPedido  implements Serializable {
 			return false;
 		return true;
 	}
-	
 
-	
 }
