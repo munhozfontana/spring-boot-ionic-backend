b37fc6e9aa4104145a9c166057833b82a8dd115e
Munhoz <munhozfontana@gmail.com>
2018-08-02 12:55:37 -0300

    validação dos de valor nulo e quantidade de aracters de Categoria

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.dto.CategoriaDTO;
 import com.udemy.cursomc.repositories.CategoriaRepository;
 import com.udemy.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,4 +57,7 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
